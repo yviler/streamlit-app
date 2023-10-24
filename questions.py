43b871fa5f639ba6cceae6e97b6aebbfabca1707
jasprelao <87517061+jasprelao@users.noreply.github.com>
2023-10-24 09:52:39 +0700

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,7 +2,9 @@ def load_question(conn, course_name, task_info, question_number):
   cursor = conn.cursor()
   question_num = str(task_info)+"_"+str(question_number)
   cursor.execute('SELECT Question FROM course_info WHERE Course = ? AND question_id = ?', (course_name, question_num))
-  result = [row[0] for row in cursor.fetchall()]
-  if result[0] is None:
-    result[0]='Tidak ada pertanyaan.'
-  return result[0]
+  #result = [row[0] for row in cursor.fetchall()]
+  result = cursor.fetchone()
+  
+  if result is None:
+    result ='Tidak ada pertanyaan.'
+  return result
