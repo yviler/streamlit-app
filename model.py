91c68961528e0fca2749a0e686511bb0bad83857
jasprelao <87517061+jasprelao@users.noreply.github.com>
2023-10-24 13:12:06 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
 
 def model_load(conn, course_name, task_info, problem):
   course_name_like = '%'+course_name+'%'
+  st.write('COURSE NAME LIKE: ', course_name_like)
   cursor = conn.cursor()
   cursor.execute('SELECT Model_Name FROM course_info WHERE Task = ? AND Course LIKE ? AND Question = ?', (task_info, course_name_like, problem))
   #result = [row[0] for row in cursor.fetchall()]
   result = cursor.fetchone()
-  
+  st.write('MODEL USED:',result[0])
   if result is None:
-    result =('aespan-bert-bilstm1_1.h5.',)
+    result =('aesindo-bert-bilstm1_1.h5.',)
     
   return result[0]
