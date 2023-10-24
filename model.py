73af57e466b422db7d59ac63770670e1a3667972
jasprelao <87517061+jasprelao@users.noreply.github.com>
2023-10-24 11:20:43 +0700

    Update model.py

--- model.py
+++ model.py
@@ -2,5 +2,10 @@
 def model_load(conn, course_name, task_info, problem):
   cursor = conn.cursor()
   cursor.execute('SELECT Model_Name FROM course_info WHERE Task = ? AND Course = ? AND Question = ?', (task_info, course_name, problem))
-  result = [row[0] for row in cursor.fetchall()]
+  #result = [row[0] for row in cursor.fetchall()]
+  result = cursor.fetchone()
+  
+  if result is None:
+    result =('aesindo-bert-bilstm1_1.h5.',)
+    
   return result[0]
