5a8c9f4cec2506bf15be5ab71e113a5d5b2fcb80
jasprelao <87517061+jasprelao@users.noreply.github.com>
2023-10-24 09:39:45 +0700

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,6 +2,9 @@
 def load_question(conn, course_name, task_info, question_number):
   cursor = conn.cursor()
   question_num = str(task_info)+"_"+str(question_number)
-  cursor.execute('SELECT Question FROM course_info WHERE Course = ? AND question_id = ?', (course_name, question_num))
-  result = [row[0] for row in cursor.fetchall()]
+  if cursor.execute('SELECT Count(*) Question FROM course_info WHERE Course = ? AND question_id = ?', (course_name, question_num))>0:
+    cursor.execute('SELECT Question FROM course_info WHERE Course = ? AND question_id = ?', (course_name, question_num))
+    result = [row[0] for row in cursor.fetchall()]
+  else:
+    result = ['Tidak ada pertanyaan.']
   return result[0]
