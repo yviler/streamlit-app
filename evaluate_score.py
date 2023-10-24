9642a9f28118f7c0a180f0809445656e4388b82f
jasprelao <87517061+jasprelao@users.noreply.github.com>
2023-10-24 11:12:45 +0700

    Update evaluate_score.py

--- evaluate_score.py
+++ evaluate_score.py
@@ -1,12 +1,12 @@
 from modeling import scoring 
 
-def evaluate_score(txt_jawaban_student, course_info, task_info, soal):
+def evaluate_score(conn, txt_jawaban_student, course_info, task_info, soal):
   student_score = 0  # Implement your scoring logic here
   bahasa = 0
   
   if course_info == 'Bahasa Inggris':
     bahasa = 1
-  student_score = scoring(txt_jawaban_student, course_info, task_info, soal, bahasa)
+  student_score = scoring(conn, txt_jawaban_student, course_info, task_info, soal, bahasa)
   #st.write("\nPredicted Score:", student_score)
   #with st.sidebar:
       #st.text_input('Nilai', value=student_score, max_chars=10, disabled=True, label_visibility="visible")
