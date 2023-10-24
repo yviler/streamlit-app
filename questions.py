5e0560330567f2a11e3bbe43ae854403adb6669d
jasprelao <87517061+jasprelao@users.noreply.github.com>
2023-10-24 09:24:20 +0700

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,6 +1,7 @@
 
-def load_question(conn, course_name, task_info):
+def load_question(conn, course_name, task_info, question_number):
   cursor = conn.cursor()
-  cursor.execute('SELECT Question FROM course_info WHERE Task = ? AND Course = ?', (task_info, course_name))
+  question_num = task_info+"_"+question_number
+  cursor.execute('SELECT Question FROM course_info WHERE Task = ? AND question_id = ?', (task_info, question_num))
   result = [row[0] for row in cursor.fetchall()]
   return result[0]
