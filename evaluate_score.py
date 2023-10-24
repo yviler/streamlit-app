ddac684da9fd7b5f04fb08cd5cdd6d7198b7d0f0
jasprelao <87517061+jasprelao@users.noreply.github.com>
2023-10-24 10:08:30 +0700

    Update evaluate_score.py

--- evaluate_score.py
+++ evaluate_score.py
@@ -2,6 +2,8 @@ from modeling import scoring
 
 def evaluate_score(txt_jawaban_student, course_info, task_info, soal):
   student_score = 0  # Implement your scoring logic here
+  bahasa = 0
+  
   if course_info == 'Bahasa Inggris':
     bahasa = 1
   student_score = scoring(txt_jawaban_student, course_info, task_info, soal, bahasa)
