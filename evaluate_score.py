d36c5c604d1e2a1cecb3a06ece12524d0b9f28e2
William Hilmy Susatyo <128654764+williamhilmysusatyo@users.noreply.github.com>
2023-10-25 08:37:52 +0700

    Update evaluate_score.py

--- evaluate_score.py
+++ evaluate_score.py
@@ -7,7 +7,7 @@ def evaluate_score(conn, txt_jawaban_student, course_info, task_info, soal):
   if course_info == 'Bahasa Inggris':
     bahasa = 1
   student_score = scoring(conn, txt_jawaban_student, course_info, task_info, soal, bahasa)
-  #st.write("\nPredicted Score:", student_score)
+  st.write("\nPredicted Score:", student_score)
   #with st.sidebar:
       #st.text_input('Nilai', value=student_score, max_chars=10, disabled=True, label_visibility="visible")
   return student_score
