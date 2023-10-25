ced8aff03feb3a612513fe2d34567e801a64f1af
William Hilmy Susatyo <128654764+williamhilmysusatyo@users.noreply.github.com>
2023-10-25 10:23:48 +0700

    Update save_score.py

--- save_score.py
+++ save_score.py
@@ -1,9 +1,9 @@
 import pandas as pd 
 
-def save_score(conn):
+def save_score(conn, score, course_info):
   prediction_score = pd.read_csv('prediction_score.csv', sep="\t", encoding="ISO-8859-1")
   string_student_score = prediction_score['score']
-  student_score = int(string_student_score)
+  student_score = score
   cursor = conn.cursor()
   cursor.execute('SELECT * FROM aes_course WHERE courseName = ?', (course_info, ))
   table = cursor.fetchall()
