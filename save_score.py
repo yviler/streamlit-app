847749bbfe19a4f0b8fd68e49df1e4173c4dd13b
William Hilmy Susatyo <128654764+williamhilmysusatyo@users.noreply.github.com>
2023-10-25 11:07:52 +0700

    Update save_score.py

--- save_score.py
+++ save_score.py
@@ -34,7 +34,7 @@ def save_score(conn, score, course_info, add_identity, task_info):
     st.write(course_id[0])
     st.write(student_id[0])
     st.write(id)
-    st.write(result[0)
+    st.write(result[0])
     cursor.execute(insert_query, (id, student_id[0], add_identity, course_id[0], assignment_id[0], txt_jawaban_student, student_score, result[0] + 1, 'Ok', 1, 'Ok'))
 
   else:
