40f2c2d6073b73ad18215ccf3454d2254a3b70e4
William Hilmy Susatyo <128654764+williamhilmysusatyo@users.noreply.github.com>
2023-10-26 23:48:51 +0700

    Update upload_pdf.py

--- upload_pdf.py
+++ upload_pdf.py
@@ -1,12 +1,19 @@
 import streamlit as st
 import fitz 
+import PyPDF2
 
 def upload_pdf(file_name):
   if file_name.type == "application/pdf":
     # Proses file PDF dengan Fitz (PyMuPDF)
     st.write(file_name)
-    words = file_name.read()
-    text = words.decode('iso-8859-1')
+    with open(file_name.name , "rb") as pdf_file:
+      pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+      text = ""
+      for page_num in range(pdf_reader.getNumPages()):
+          page = pdf_reader.getPage(page_num)
+          text += page.extractText()
+    #words = file_name.read()
+    #text = words.decode('iso-8859-1')
 
     #doc = fitz.open(file_name.name)
     #text = ''
