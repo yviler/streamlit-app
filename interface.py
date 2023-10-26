341bed9e27f4c5c576a3cfabdeec96f6133ce040
William Hilmy Susatyo <128654764+williamhilmysusatyo@users.noreply.github.com>
2023-10-26 12:09:14 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -112,24 +112,7 @@ if LOGGED_IN == True:
         save_score(txt_jawaban_student, score, course_info, add_identity, task_info)
         
     if btn_upload:
-        st.write("Upload answer via file:")
-        uploaded_file = st.file_uploader("Choose a file", type=["pdf","png", "JPG"], accept_multiple_files = False)
-        if uploaded_file.type == "application/pdf":
-            # Proses file PDF dengan Fitz (PyMuPDF)
-            doc = fitz.open(uploaded_file)
-            text = ''
-            for page in doc:
-                page_text = page.get_text()
-                text += page_text	
-        else:
-            st.text("Unsupported file format. Please upload a PDF, PNG, or JPG file.")
-    else:
-        doc = fitz.open(uploaded_file)
-        text = ''
-        for page in doc:
-                    page_text = page.get_text()
-                    text += page_text
-        pymupdf_test = text
+        upload_pdf()
 
 
     #show output
