37051e9fb53a8ac593a84d88f7336b44d543961d
William Hilmy Susatyo <128654764+williamhilmysusatyo@users.noreply.github.com>
2023-10-26 23:19:32 +0700

    Update upload_pdf.py

--- upload_pdf.py
+++ upload_pdf.py
@@ -5,11 +5,12 @@ def upload_pdf(file_name):
   if file_name.type == "application/pdf":
     # Proses file PDF dengan Fitz (PyMuPDF)
     st.write(file_name)
-    doc = fitz.open(file_name.name)
-    text = ''
-    for page in doc:
-        page_text = page.get_text()
-        text += page_text	
+    text = file_name.read()
+    #doc = fitz.open(file_name.name)
+    #text = ''
+    #for page in doc:
+        #page_text = page.get_text()
+        #text += page_text	
   else:
         st.write("Unsupported file format. Please upload a PDF, PNG, or JPG file.")
         text = 'Salah Input'
