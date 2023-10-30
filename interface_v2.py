0d842341bd2360cca42092b41c4cfd25dc4f27bb
jasprelao <87517061+jasprelao@users.noreply.github.com>
2023-10-30 22:23:45 +0700

    Update interface_v2.py

--- interface_v2.py
+++ interface_v2.py
@@ -89,7 +89,8 @@ if LOGGED_IN == True:
     
     txt_jawaban_student = st.text_area("Answer", "Tidak Menjawab", height=400)
     
-    st.write(f'You wrote {len(txt_jawaban_student)} characters.')
+    #st.write(f'You wrote {len(txt_jawaban_student)} characters.')
+    
     col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
     
     with col1:
